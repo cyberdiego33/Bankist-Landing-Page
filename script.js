f0da6ee6aa412a052c987d7ff92fd2f81c8674a6
Diego <iwujigoodluck5@gmail.com>
2025-06-21 18:43:17 +0000

    added smooth scrollings

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const modal = document.querySelector(".modal");
 
 document.querySelectorAll(".openModal").forEach((close) => {
-    console.log(close);
+  console.log(close);
   close.addEventListener("click", () => {
     modal.classList.remove("hidden");
     modal.classList.add("flex");
   });
 });
 
-
 document.querySelectorAll("#closeModal").forEach((close) => {
-    console.log(close);
+  console.log(close);
   close.addEventListener("click", () => {
     modal.classList.remove("flex");
     modal.classList.add("hidden");
   });
+});
+
+const btnFeature = document.querySelector("#btnScrolltoFeature");
+const features = document.querySelector("#features");
+
+btnFeature.addEventListener("click", (e) => {
+  features.scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector("#navBar").addEventListener("click", (e) => {
+  if (e.target.tagName === "A") {
+    const target = document.querySelector(e.target.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  }
 });
